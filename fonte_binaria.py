d810b57ab8cc29bf084a772f9d3956d946ba957a
Debora Amorim <debora.amorim@sonner.com.br>
2021-03-29 22:07:26 -0300

    Refatorando arquivo de fonte_binaria.py

--- fonte_binaria.py
+++ fonte_binaria.py
@@ -3,38 +3,40 @@
 import numpy as numpy
 import matplotlib.pyplot as plot
 
-plot.close('all')
-
-duracao_simbolos = 16
-quantidade_simbolos = 60000
-probabilidade = 0.5
-
-aleatorio = numpy.random.rand(quantidade_simbolos)
-aleatorio[numpy.where(aleatorio >= probabilidade)] = 1
-aleatorio[numpy.where(aleatorio < probabilidade)] = 0
-
-quantidadeZeros = 0
-quantidadeUms = 0
-
-for amostra in aleatorio:
-    if amostra == 0:
-        quantidadeZeros += 1
-    else:
-        quantidadeUms += 1
+def gera_analise_simbolos(array):
+    quantidadeZeros = 0
+    quantidadeUms = 0
+    for amostra in array:
+        if amostra == 0:
+            quantidadeZeros += 1
+        else:
+            quantidadeUms +=1
+    print("De um total de", len(array), "símbolos", round(quantidadeZeros/len(array)*100,2), "% foram 0 (Zero) e", round(quantidadeUms/len(array)*100,2), "% foram 1 (Um)")
 
 
-print("De um total de", quantidade_simbolos, "símbolos", quantidadeZeros, "foram 0 (Zero) e", quantidadeUms, "foram 1 (Um)")
+def plota_simbolos(array, quantidade_simbolos, duracao_simbolos):
+    sinal = numpy.zeros(quantidade_simbolos * duracao_simbolos)
+    sinalUm = numpy.where(array == 1)
 
- # gerando sinal
+    for i in sinalUm[0]:
+        temp = int(i * duracao_simbolos)
+        sinal[temp: temp + duracao_simbolos] = 1
 
-sinal = numpy.zeros(quantidade_simbolos*duracao_simbolos)
-id_n = numpy.where(aleatorio == 1)
+    plot.plot(sinal);
+    plot.title('Fonte Binária Sem Memória')
+    plot.xlabel('Bits transmitidos');
+    plot.ylabel('Amplitude')
+    plot.show()
 
-for i in id_n[0]:
-    temp = int(i*duracao_simbolos)
-    sinal[temp: temp+duracao_simbolos] = 1
+
+def gera_simbolos(quantidade_simbolos = 0, probabilidade_zero = 0, duracao_simbolos = 0):
+    plot.close('all')
+
+    aleatorio = numpy.random.rand(quantidade_simbolos)
+    aleatorio[numpy.where(aleatorio >= probabilidade_zero)] = 1
+    aleatorio[numpy.where(aleatorio < probabilidade_zero)] = 0
+    gera_analise_simbolos(aleatorio)
+    if duracao_simbolos > 0:
+        plota_simbolos(aleatorio, quantidade_simbolos, duracao_simbolos)
 
 
-plot.plot(sinal); plot.title('Fonte Binária Sem Memória Equiprovável')
-plot.xlabel('Duração (seg)');plot.ylabel('Amplitude')
-plot.show()
---
+++ main.py
@@ -0,0 +1,3 @@
+from fonte_binaria import gera_simbolos
+
+gera_simbolos(10000, 0.5)
